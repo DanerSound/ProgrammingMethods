9777babefeb1d79063532a309e49fa5430ef33ca
DanerSound <andre.barreto28@gmail.com>
2018-10-08 19:06:20 +0200

    added 1 more test control about the String

--- ProgrammingMethods/src/test/java/base/BadCodeTest.java
+++ ProgrammingMethods/src/test/java/base/BadCodeTest.java
@@ -4,9 +4,15 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BadCodeTest {
+	
+	@Test
+	public void CorrectString() {
+		String Mantra="I'll not write bad code and also I'll use TDD";
+		assertEquals(Mantra, BadCode.getMantra());
+	}
 
 	@Test
-	public void mantra() {		
+	public void StringLenght() {		
 			controloLenght(BadCode.getMantra(),45);	
 	}
 	
