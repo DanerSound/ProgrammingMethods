f46dbd8397f47b8f87ef2d3556aa7434e604ffb3
DanerSound <andre.barreto28@gmail.com>
2018-10-10 17:58:21 +0200

    added interface and some dont know how to test the new methods

--- ProgrammingMethods/src/main/java/base/BinaryExp.java
+++ ProgrammingMethods/src/main/java/base/BinaryExp.java
@@ -1,34 +1,38 @@
 package base;
 
-public class BinaryExp {
+public class BinaryExp implements Exp{
 	
-	private Costant opsx;
-	private Costant opdx;
+	private Costant opSX;
+	private Costant opDX;
 
 	public BinaryExp(Costant exp1, Costant exp2) {
 		super();
-		this.opsx = exp1;
-		this.opdx = exp2;
+		this.opSX = exp1;
+		this.opDX = exp2;
 	}
 
 	public Costant getExp1() {
-		return opsx;
+		return opSX;
 	}
 
 	public void setExp1(Costant exp1) {
-		this.opsx = exp1;
+		this.opSX = exp1;
 	}
 
 	public Costant getExp2() {
-		return opdx;
+		return opDX;
 	}
 
 	public void setExp2(Costant exp2) {
-		this.opdx = exp2;
+		this.opDX = exp2;
 	}
 
 	public static int getValutation(int exp1, int exp2) {
 		return exp1+exp2;
+	}
+
+	public int valutation() {
+		return opSX.getValue()+opDX.getValue();
 	}
 
 }
