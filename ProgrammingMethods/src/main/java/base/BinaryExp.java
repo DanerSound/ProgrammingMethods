f8e91a0d52ace779bf29c07e339d4662453d45cd
DanerSound <andre.barreto28@gmail.com>
2018-10-10 17:47:58 +0200

    rename for clarity

--- ProgrammingMethods/src/main/java/base/BinaryExp.java
+++ ProgrammingMethods/src/main/java/base/BinaryExp.java
@@ -2,29 +2,29 @@ package base;
 
 public class BinaryExp {
 	
-	private Costant exp1;
-	private Costant exp2;
+	private Costant opsx;
+	private Costant opdx;
 
 	public BinaryExp(Costant exp1, Costant exp2) {
 		super();
-		this.exp1 = exp1;
-		this.exp2 = exp2;
+		this.opsx = exp1;
+		this.opdx = exp2;
 	}
 
 	public Costant getExp1() {
-		return exp1;
+		return opsx;
 	}
 
 	public void setExp1(Costant exp1) {
-		this.exp1 = exp1;
+		this.opsx = exp1;
 	}
 
 	public Costant getExp2() {
-		return exp2;
+		return opdx;
 	}
 
 	public void setExp2(Costant exp2) {
-		this.exp2 = exp2;
+		this.opdx = exp2;
 	}
 
 	public static int getValutation(int exp1, int exp2) {
